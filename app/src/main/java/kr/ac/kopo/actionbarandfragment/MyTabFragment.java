dfbc94d1c6ccfdc956329508c67c5e1448ae4cd5
smcmfmf <hk0831bae@gmail.com>
2025-06-02 15:15:02 +0900

    Chapter 7 ActionBar, Fragment를 이용한 Tab 예제 (이미지 출력 기능 추가)

--- app/src/main/java/kr/ac/kopo/actionbarandfragment/MyTabFragment.java
+++ app/src/main/java/kr/ac/kopo/actionbarandfragment/MyTabFragment.java
@@ -2,9 +2,12 @@ package kr.ac.kopo.actionbarandfragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -28,17 +31,26 @@ public class MyTabFragment extends Fragment {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT); // width, height
         baseLayout.setLayoutParams(params); // 정의한 레이아웃 설정을 사용함
 
+        ImageView img1 = new ImageView(super.getActivity());
+        img1.setImageResource(R.drawable.spring);
+        img1.setAdjustViewBounds(true);
+
+        baseLayout.addView(img1);
+
         if (tabName.equals("Spring"))
         {
             baseLayout.setBackgroundColor(Color.GREEN);
+            img1.setImageResource(R.drawable.spring);
         }
         if (tabName.equals("Summer"))
         {
             baseLayout.setBackgroundColor(Color.CYAN);
+            img1.setImageResource(R.drawable.summer);
         }
         if (tabName.equals("Fall"))
         {
             baseLayout.setBackgroundColor(Color.rgb(204,153,102));
+            img1.setImageResource(R.drawable.fall);
         }
 
         return baseLayout; // 레이아웃을 리턴(출력)
